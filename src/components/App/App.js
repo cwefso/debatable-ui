a70a75158e90bf874f2802ea4e961fadc78b85d0
Charles Wefso <cwefso@gmail.com>
2020-10-06 11:27:05 -0600

    sets up PubNub room create

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,78 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./App.scss";
 import { Switch, Route, withRouter, NavLink } from "react-router-dom";
 import Topic from '../Topic/Topic'
+import Lobby from '../Lobby/Lobby'
+import PubNubReact from 'pubnub-react';
+import shortid from 'shortid';
 
 
-function App() {
+class App extends Component { 
+	constructor(props) {  
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: "pub-c-7b46f3ec-deeb-4817-9e4d-146bbdd69733", 
+      subscribeKey: "sub-c-c9d2f832-01a4-11eb-88da-261b8c980873"    
+    });
+    
+    this.state = {
+      player: 0,
+      isPlaying: false,
+      isRoomCreator: false,
+      isDisabled: false,
+      myTurn: false,
+    };
+
+    this.lobbyChannel = null;
+    this.gameChannel = null;
+    this.roomId = null;    
+    this.pubnub.init(this);
+  }
+
+	componentWillUnmount() {
+    this.pubnub.unsubscribe({
+      channels : [this.lobbyChannel, this.gameChannel]
+    });
+	}
+	
+	onPressCreate = (e) => {
+    // Create a random name for the channel
+    this.roomId = shortid.generate().substring(0,5);
+    this.lobbyChannel = 'tictactoelobby--' + this.roomId;
+
+    this.pubnub.subscribe({
+      channels: [this.lobbyChannel],
+      withPresence: true
+    });
+
+    this.setState({
+      player: 1,
+      isRoomCreator: true,
+      isDisabled: true, // Disable the 'Create' button
+      myTurn: true, // Room creator makes the 1st move
+    });   
+	}
+	
+	  // Reset everything
+  endGame = () => {
+    this.setState({
+      piece: '',
+      isPlaying: false,
+      isRoomCreator: false,
+      isDisabled: false,
+      myTurn: false,
+    });
+
+    this.lobbyChannel = null;
+    this.gameChannel = null;
+    this.roomId = null;  
+
+    this.pubnub.unsubscribe({
+      channels : [this.lobbyChannel, this.gameChannel]
+    });
+  }
+
+	render(){
 	const mainPage = (
 		<div className="App">
 			<header className="App-header">
@@ -13,9 +81,10 @@ function App() {
 				</div>
 				<section className="buttons">
 					<NavLink
-						to={"/"}
+						to={"/lobby"}
 						className="create"
 						style={{ textDecoration: "none" }}
+						onClick={(e) => this.onPressCreate()}
 					>
 						Create
 					</NavLink>
@@ -34,8 +103,11 @@ function App() {
 				</div>
 			} />
 			<Route exact path="/topics" render={() => <Topic />} />
+			<Route exact path="/lobby" render={() => <Lobby />} />
+			
 		</Switch>
 	);
+		}
 }
 
 export default withRouter(App);
---
+++ src/components/Lobby/Lobby.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Link } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+
+function Lobby() {
+
+	return ;
+}
+
+export default Lobby;
+
+// Lobby.propTypes = {
+// }
